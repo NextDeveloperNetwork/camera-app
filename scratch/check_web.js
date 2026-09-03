3e882c65125df477c17f05688ddca4b9fb9490a2
Elvis Simaku <g3albania.elvis@gmail.com>
2026-09-03 22:43:36 +0200

    chore: clean up temporary test scripts

--- scratch/check_web.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-http.get('http://192.168.1.10/', (res) => {
-  let d = '';
-  res.on('data', (c) => (d += c));
-  res.on('end', () => {
-    console.log(d.slice(0, 1000));
-  });
-});
